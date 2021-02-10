9b54c9e904bb56e21b719617783a883bdd277d45
长238 <1195359927@qq.com>
2021-02-10 13:58:17 +0800

    QT版Word文档

---
+++ wps001/childwnd.h
@@ -0,0 +1,43 @@
+﻿#ifndef CHILDWND_H
+#define CHILDWND_H
+
+#include <QTextEdit>
+#include <QFileDialog>
+#include <QTextDocumentWriter>
+#include <QMessageBox>
+#include <QCloseEvent>
+#include <QTextBlockFormat>
+#include <QtWidgets>
+
+class ChildWnd : public QTextEdit
+{
+    Q_OBJECT
+public:
+    ChildWnd();
+    QString m_CurDocPath;       //当前文档路径
+    void newDoc();              //新建文档
+    QString getCurDocName();    //文件路径中提取文档名
+    bool loadDoc(const QString &docName);
+    void setCurDoc(const QString &docName);
+    bool saveDoc();             //保存文档
+    bool saveAsDoc();           //另存为
+    bool saveDocOpt(QString &docName);
+    void setFormatOnSelectedWord(const QTextCharFormat &fmt);
+    void setAlignOfDocumentText(int aligntype);
+    void setparaSyle(int pStyle);
+
+protected:
+    void closeEvent(QCloseEvent *event);
+private:
+    bool promptSave();
+
+private slots:
+    void docBeModified();       //文档修改时，窗口的标题添加'*'
+
+private:
+    bool m_bSaved;              //文档是否保存
+
+
+};
+
+#endif // CHILDWND_H
---
+++ wps001/mainwindow.h
@@ -0,0 +1,119 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "childwnd.h"
+#include <QCloseEvent>
+#include <QSignalMapper>
+#include <QMdiSubWindow>
+#include <QColorDialog>
+#include <QtPrintSupport/QPrinter>
+#include <QtPrintSupport/QPrintDialog>
+#include <QtPrintSupport/QPrintPreviewDialog>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void initMainWindow();
+    void docNew();
+    void docOpen();
+    void docSave();
+    void docSaveAs();
+    void docPrint();
+    void docPrintPreview();
+    void docUndo();
+    void docRedo();
+    void docCut();
+    void docCopy();
+    void docPaste();
+    void textBold();
+    void textItalic();
+    void textUnderline();
+    void textFamily(const QString &fmly);
+    void textSize(const QString &ps);
+    void textColor();
+    void paraStyle(int nStyle);
+
+private slots:
+    void on_newAction_triggered();
+    void refreshMenus();
+    void addSubWndMenu();
+    void PrintPreview(QPrinter * printer);
+    void on_closeAction_triggered();
+
+    void on_closeAllAction_triggered();
+
+    void on_titleAction_triggered();
+
+    void on_cascadeAction_triggered();
+
+    void on_nextAction_triggered();
+
+    void on_previousAction_triggered();
+
+    void setActiveSubWindow(QWidget * wnd);
+
+    void on_openAction_triggered();
+
+    void on_saveAction_triggered();
+
+    void on_saveAsAction_triggered();
+
+    void on_undoAction_triggered();
+
+    void on_redoAction_triggered();
+
+    void on_cutAction_triggered();
+
+    void on_copyAction_triggered();
+
+    void on_pasteAction_triggered();
+
+    void on_boldAction_triggered();
+
+    void on_italicAction_triggered();
+
+    void on_underlineAction_triggered();
+
+    void on_fontComboBox_activated(const QString &arg1);
+
+    void on_sizeComboBox_activated(const QString &arg1);
+
+    void on_leftAction_triggered();
+
+    void on_rightAction_triggered();
+
+    void on_centerAction_triggered();
+
+    void on_justifyAction_triggered();
+
+    void on_colorAction_triggered();
+
+    void on_comboBox_activated(int index);
+
+    void on_printAction_triggered();
+
+    void on_printPreviewAction_triggered();
+
+protected:
+    //重载关闭事件
+    void closeEvent(QCloseEvent *event);
+private:
+    void formatEnaled();
+    ChildWnd *activateChildWnd();//获取活动的子窗口
+    QMdiSubWindow* findChildWnd(const QString &docName);
+private:
+    Ui::MainWindow *ui;
+
+    QSignalMapper *m_WndMapper;//信号映射器
+
+};
+#endif // MAINWINDOW_H
